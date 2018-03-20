75474aab34af0e0aee6b2dcd63eb4f0c9279d115
Matteo Crippa <matteo@boostco.de>
2018-03-21 00:27:35 +0100

    improved code

--- scripts/images.js
+++ scripts/images.js
@@ -2,6 +2,7 @@
 const ui = require('../lib/ui');
 const utils = require('../lib/utils');
 const path = require('path');
+const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../tmp/data.json'), 'utf8'));
 
